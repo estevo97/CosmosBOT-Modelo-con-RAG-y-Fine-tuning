fbf31e99f29591c02471082cef7654ac3f31facf
Estevo Arias García <estevoag@gmail.com>
2025-02-03 17:11:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ apikey = st.secrets.get("clave")
 import faiss
 import json
 index = faiss.read_index("faiss_index.index")
-with open("document_texts.json", "r") as f:
+with open(url_document_json, "r") as f:
     documentos = json.load(f)
 
 
